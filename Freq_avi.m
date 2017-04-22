3a9e0ed524cfeb707b69e8628cb18ada739707b2
Siyu He <copper.helium45@gmail.com>
2017-04-22 22:40:31 +0800

    Update Freq_avi.m

--- Freq_avi.m
+++ Freq_avi.m
@@ -1,6 +1,5 @@
 % This is a ".m" file to determine the frequency among time domain for avi
-% files. Use the "simpleConvertTDMS.m" or "ConvertTDMS.m" first to convert
-% the TDMS files to mat format. To determine the steps, pottslab package is
+% files. To determine the steps, pottslab package is
 % used here so JavaPath should be set first by run the "installPottslab.m"
 % in the "Pottslab0.5" folder.This version include analysis of dissociation
 % and recovery periods.
@@ -33,36 +32,28 @@ lengthFiles=length(Video_Files);
     %clearvars -except kk Files lengthFiles folder_name
     clc;
 %% load information of the video.
-%h=waitbar(0,'Loading video, please wait...');
 video_name = Video_Files(kk).name;
 video_path = strcat(folder_name,'\',video_name);
 obj = VideoReader(video_path); %read the objects
 % get the height and width for the video
 vidHeight = obj.Height;
 vidWidth = obj.Width;
-% set a struct for store the data
-s = struct('cdata',zeros(vidHeight,vidWidth,3,'uint8'),...
-    'graydata',zeros(vidHeight,vidWidth,1,'uint8'),...
-    'denoi_graydata',zeros(vidHeight,vidWidth,1,'uint8'),...
-    'dvalue',zeros(vidHeight,vidWidth,1,'double'));
-   numFrame = 1; 
-
-while hasFrame(obj)
-    %waitbar(numFram/)
-    s(numFrame).cdata = readFrame(obj);
-    s(numFrame).graydata = rgb2gray(s(numFrame).cdata); % convert rgb to gray image
-    % use the averaging filter mode to denoise the image
-    s(numFrame).denoi_graydata = filter2(fspecial('average',7),s(numFrame).graydata)/255; 
-   %s(numFrame).bwdata = imbinarize(s(numFrame).denoi_graydata,thre);
+vidFrames = obj.Duration*obj.FrameRate;
+ss=zeros(vidHeight,vidWidth,3,vidFrames);
    
-    % convert the unit8 format to double for the gray image
-    s(numFrame).dvalue = im2double(s(numFrame).denoi_graydata);
-    numFrame = numFrame+1;
+for i=1:vidFrames
+    clc
+    disp(['Loading video ' strcat(num2str(i/vidFrames),'%')]);
+    ss(:,:,:,i) = readFrame(obj);
 end
-numFrame = numFrame-1; % minus one since count one more for the number of Frame
 
-    %% Load related tdms for the video and get the information of the fields state.    
-    %waitbar(1,'Loading the tdms files, please wait...');
+ssgray=0.2989*ss(:,:,1,:)+ 0.5870 * ss(:,:,2,:) + 0.1140 * ss(:,:,3,:);
+clear ss;
+ssgray=reshape(ssgray,[vidHeight vidWidth vidFrames]);
+% for i=1:numFrame
+%     ssgray(:,:,i)=filter2(fspecial('average',7),ssgray(:,:,i))/255;
+% end
+%% Load related tdms for the video and get the information of the fields state.
     related_tdms= Tdms_Files(kk).name;
     tdms_path = strcat(folder_name,'\',related_tdms);
     load(tdms_path);
@@ -77,24 +68,36 @@ numFrame = numFrame-1; % minus one since count one more for the number of Frame
     %ended. Frequency calculated between these two.    
     ON_pairs = [OnOff(1:2:end) OnOff(2:2:end)];
     OFF_pairs = [OnOff(2:2:end-2) OnOff(3:2:end)];
-    ON_cnt = (ON_pairs(3,2)-ON_pairs(3,1)); % # samples in each ON event
-
-    %!!!Notice!!! : some data need (OFF_pairs(1,2)-OFF_pairs(1,1))
-    OFF_cnt = (OFF_pairs(3,2)-OFF_pairs(3,1)); % # samples in each OFF event 
+     numoff=size(OFF_pairs,1);
+    if numoff>=3 
+        numoff=3;
+    end
+      
+    numon=size(ON_pairs,1);
+    if numon>=3 
+        numon=3;
+    end
+    ON_cnt = (ON_pairs(numon,2)-ON_pairs(numon,1)); % # samples in each ON event
     
+    %some data need (OFF_pairs(1,2)-OFF_pairs(1,1))
+    OFF_cnt = (OFF_pairs(numoff,2)-OFF_pairs(numoff,1)); % # samples in each OFF event 
+    
+
     % The matrix above missed the last OFF event immediately following the last
     % ON event and before the recovery starts (two seconds after the last ON).
     % Adding that bit to the Off_pairs here.
     rec_flag = 0;
     if size(F_state,1)-ON_pairs(end,2)>OFF_cnt && F_state(end)==0
         %some data need (OFF_pairs(1,2)-OFF_pairs(1,1))
-        OFF_pairs = vertcat([1 OnOff(1)],OFF_pairs,[OnOff(end) OnOff(end)+(OFF_pairs(3,2)-OFF_pairs(3,1))]);
+        OFF_pairs = vertcat([1 OnOff(1)],OFF_pairs,[OnOff(end) OnOff(end)+(OFF_pairs(numoff,2)-OFF_pairs(numoff,1))]);
         rec_flag = 1;
     elseif size(F_state,1)-ON_pairs(end,2)<OFF_cnt && F_state(end)==0
         OFF_pairs = vertcat([1 OnOff(1)],OFF_pairs);
     else
         OFF_pairs = vertcat([1 OnOff(1)],OFF_pairs);
     end
+     
+    
      % get REC pairs
      REC_array = OFF_pairs(end,2):OFF_cnt:size(F_state,1);
      REC_pairs = [REC_array(1:end-1)' REC_array(2:end)'];
@@ -105,20 +108,18 @@ REC_pairs = ((REC_pairs-1)./fs);
 
 
  %% Tracking the center of the cell in the video
-waitbar(3,'Loading the tdms files, please wait...');
-pos=zeros(numFrame,2); %two columns, the first is x position, the second is y position.
-thre=0.2; % the threshhold for center calculating 
-for i=1:numFrame
-    pic=s(i).dvalue;
-    pos(i,:)=center(pic,thre); % center is a customed function to find the center of the cell in the image.
+pos=zeros(vidFrames,2);
+for i=1:vidFrames
+clc
+disp(['Processing video ' strcat(num2str(i/vidFrames),'%')])
+pic=ssgray(:,:,i);
+pos(i,:)=center(pic);
 end
-
+clear ssgray;
 %% Apply wavelet transform 
-value_min=2;
-T = obj.Duration/numFrame;             % Sampling period
-Fs = 1/T;            % Sampling frequency of video
-L = numFrame;             % Length of signal
-t = (0:L-1)*T;        % Time vector
+T = obj.Duration/vidFrames;             % Sampling period
+Fs = obj.FrameRate;            % Sampling frequency of video         % Length of signal
+t = (0:vidFrames-1)*T;        % Time vector
 wlet = 'morse';              % name of the mother wavelet
 Scale_min=0;  % Set the minimum of frequency to 0 first, and search a proper one in the following steps
 Scale_max=1e7; % Set the maximum of frequency to a large value first, and search a proper one in the following steps
@@ -132,6 +133,26 @@ coepow=abs(coefsy)+abs(coefsx);
 coepow(Scale_max+1:end,:)=[];
 coepow(1:Scale_min-1,:)=[];
 freq =freqx(Scale_min:1:Scale_max);
+
+%% Offset determination 
+ % Determine the offset between the tdms data and video data. video always
+ % has some extra recording due to some time delay between turning on the
+ % video recording and turning on the labview program.
+  
+ signal = pos(1:10*Fs,:); % Take first 10 seconds of the video data
+ signal = signal - movmean(signal,5);
+ wl = 1;
+ num_switch = 10;
+ while num_switch > 1
+ [up,lo] = envelope(signal,wl,'rms');
+ S = mean(up-lo,2);
+ S_bin = (S > (max(S)+min(S))/2);
+ S_switch = diff(S_bin); % find if the field was switched
+ num_switch = sum(abs(S_switch));
+ F_ON_vid = find(S_switch,1); % Index of the video frame when field was switched on
+ wl = wl+1;
+ end
+ OffSet = F_ON_vid - find((t>OFF_pairs(1,2)),1,'first')-1;
 %% Dealing with dissociation period
 OFF_period=zeros(size(OFF_pairs,1),2); % OFF pairs in the video 
 coepow_off=[]; % store the coefficients of WT for dissociation 
@@ -139,17 +160,21 @@ real_time_off=[]; % correspond the real time for each period
 inter_off=[]; % dashed lines inserted to denote each period
 
 for i=1: size(OFF_pairs,1)
-OFF_period(i,1) = find((t>=OFF_pairs(i,1)),1,'first'); % find the start point for OFF period i
-OFF_period(i,2) = find((t<=OFF_pairs(i,2)),1,'last'); % find the end poingt for OFF period i
+OFF_period(i,1) = find((t>=OFF_pairs(i,1)),1,'first')+OffSet; % find the start point for OFF period i
+OFF_period(i,2) = find((t<=OFF_pairs(i,2)),1,'last')+OffSet; % find the end poingt for OFF period i
+end
+
+for i=1:size(OFF_pairs,1)
 coepow_off=[coepow_off coepow(:,OFF_period(i,1):OFF_period(i,2))]; % get the coefficients for the OFF period i
 tt=linspace(OFF_pairs(i,1),OFF_pairs(i,2),OFF_period(i,2)-OFF_period(i,1)+1); % get the real time
 real_time_off=[real_time_off tt]; % collect real time for fields off
 inter_off=[inter_off length(real_time_off)]; % collect lines position 
 end;
+ inter_off=inter_off-1;
  inter_off=[0 inter_off]; % insert the start time.
  inter_off(end)=[];
  %% peak finder for OFF
-        value_min=3; % threshold of peak
+        value_min=1; % threshold of peak
         peak_sampling_off= 1; % sampling frequency for peak finder
         real_peak_off=[];
         % a customed function for peak finder
@@ -157,6 +182,7 @@ end;
        
         % correspond the real value of peaks for the WT scale
         tt = isnan(peak_off);
+        
         for i=1: length(peak_off)
         if tt(i) == 0
             scale= peak_off(i);     
@@ -171,19 +197,20 @@ coepow_rec=[]; % store the coefficients of WT for recovery
 real_time_rec=[]; % correspond the real time for each period
 inter_rec=[];
 for i=1: size(REC_pairs,1)
-REC_period(i,1) = find((t>=REC_pairs(i,1)),1,'first'); % find the start point for REC period i
-REC_period(i,2) = find((t<=REC_pairs(i,2)),1,'last'); % find the end poingt for REC period i
+REC_period(i,1) = find((t>=REC_pairs(i,1)),1,'first')+OffSet; % find the start point for REC period i
+REC_period(i,2) = find((t<=REC_pairs(i,2)),1,'last')+OffSet; % find the end poingt for REC period i
+end
+for i=1:size(REC_pairs,1)
 coepow_rec=[coepow_rec coepow(:,REC_period(i,1):REC_period(i,2))]; % get the coefficients for the REC period i
 tt=linspace(REC_pairs(i,1),REC_pairs(i,2),REC_period(i,2)-REC_period(i,1)+1); % get the real time
 real_time_rec=[real_time_rec tt]; % collect real time for fields recovery
 inter_rec=[inter_rec length(real_time_rec)]; % collect lines position 
-end;
-
+end
 
 
  %% peak finder for REC
-        value_min=3; % threshold of peak
-        peak_sampling_rec= 20; % sampling frequency for peak finder
+        value_min=1; % threshold of peak
+        peak_sampling_rec= 50; % sampling frequency for peak finder
         real_peak_rec=[];
         % a customed function for peak finder
         peak_rec=wltpeakfinder(coepow_rec(:,1:peak_sampling_rec:end),3,value_min);
@@ -225,7 +252,7 @@ y=freq(1:end);
   title(name);
   axis tight;
  % set x label for each pr
-  label_step=size(OFF_pairs,1)/8; % the interbal of x axis label for off periods.
+  label_step=size(OFF_pairs,1)/5; % the interbal of x axis label for off periods.
    % plot peak  
   inter_off=inter_off./spec_sampling;
   z=40*ones(1,length(peak_off));
@@ -247,7 +274,7 @@ end
 %% Recovery
 % set the grid for the wavelet spectrum
 set(gcf,'CurrentAxes',A2)
-spec_sampling=20; % sampling frequency for the wavelet spectrum
+spec_sampling=50; % sampling frequency for the wavelet spectrum
 x=(linspace(1,size(coepow_rec(:,1:spec_sampling:end),2),size(coepow_rec(:,1:spec_sampling:end),2)));
 y=freq(1:end);
 [X Y]=meshgrid(x,y);
@@ -287,6 +314,6 @@ set(gcf,'PaperType','A4')
     set(gcf,'PaperPosition',[xLeft yTop xSize ySize])
     set(gcf,'Position',[100 100 xSize*50 ySize*50])
     set(gcf,'PaperOrientation','landscape');
-    plot_name = strcat(related_tdms(1:end-4),'video.png');
+    plot_name = strcat(related_tdms(1:end-4),'video21.png');
     saveas(gcf,plot_name)
-   end  
+   end
